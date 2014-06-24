6cc32a6eb5c8b77358d02b539eb4bb9d82900045
sheldon <sheldon.xu@ymail.com>
2014-06-24 13:49:38 +0800

    rename apply_id to apply_detail_id

--- src/main/java/com/eabax/hospital/integration/task/Sqls.java
+++ src/main/java/com/eabax/hospital/integration/task/Sqls.java
@@ -27,7 +27,7 @@ class Sqls {
   public static String selMmActivities = "select * from MmActivity where update_time > ? order by id";
   
   public static String selUnappliedJspActivities = 
-      "select apply_id from JspActivity where is_apply = -1 and update_time > ?";
+      "select apply_detail_id from JspActivity where is_apply = -1 and update_time > ?";
   
   public static String insInOutActivity =
       "insert into itemactivity (lngactivityid, lngactivitytypeid, lngreceipttypeid, lngtemplateid, "
